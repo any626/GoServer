8a042508da3feda55f4cf9c589663654c515c0fb
brwhale <lazorpwnage@gmail.com>
2017-02-06 22:51:40 -0800

    front end edit and reply

--- content/js/reply.js
+++ content/js/reply.js
@@ -1,12 +1,19 @@
 $(function(){
     $('.reply').click(function () {
-        alert("Clicked reply on post id:" + $(this).attr('data-id'));
+        $(this).parent().parent().after($('#reply-box').html());
+    });
+    $('.post-list').on("click", ".reply-cancel-button", function(){
+        $(this).parent().parent().parent().replaceWith('');
     });
     $('.edit').click(function () {
         var comment = $(this).parent().prev('.comment');
         var oldContent = comment.html();
         comment.replaceWith($('#edit-box').html())
         $(this).parent().prev().children(':first-child').children('.edit-content').html(oldContent);
-        //alert("Clicked edit on post id:" + $(this).attr('data-id'));
+        $(this).parent().prev().children(':last-child').children('.edit-cancel-button').attr('data-oldContent',oldContent);
+    });
+    $('.post-list').on("click", ".edit-cancel-button", function(){
+        var oldContent = ($(this).attr('data-oldContent'));
+        $(this).parent().parent().replaceWith('<div class="comment">'+oldContent+'</div>');
     });
 });

1c2be7f33153e4658af7fa23d83c94906b429036
brwhale <lazorpwnage@gmail.com>
2017-02-12 22:35:02 -0800

    inline images

--- content/js/reply.js
+++ content/js/reply.js
@@ -1,4 +1,5 @@
 $(function(){
+    // start a reply
     $('.reply').click(function () {
         var $this = $(this);
         $this.parent().after($('#reply-box').html());
@@ -8,25 +9,29 @@ $(function(){
             return value + type + "/" + $this.attr('data-id');
         });
     });
+    // cancel replying
     $('.post-list').on("click", ".reply-cancel-button", function(){
         $(this).parent().parent().parent().replaceWith('');
     });
+    // start editing
     $('.edit').click(function () {
         var comment = $(this).parent().prev('.comment');
         var oldContent = comment.html();
-        comment.replaceWith($('#edit-box').html())
+        comment.replaceWith($('#edit-box').html());
         var $this = $(this);
         $this.parent().parent().children('form').children('div').children('textarea').focus();
-        $this.parent().prev().children(':first-child').children('.edit-content').html(oldContent);
+        $this.parent().prev().children(':first-child').children('.edit-content').html(oldContent.replace(/<img src="|">/g,""));
         $this.parent().prev().children(':last-child').children('.edit-cancel-button').attr('data-oldContent',oldContent);
         $this.parent().parent().children('form').attr('action', function(i, value) {
             return value + $this.attr('data-type') + "/" + $this.attr('data-id');
         });
     });
+    // cancel editing
     $('.post-list').on("click", ".edit-cancel-button", function(){
         var oldContent = ($(this).attr('data-oldContent'));
         $(this).parent().parent().replaceWith('<div class="comment">'+oldContent+'</div>');
     });
+    // hides/shows child comments
     $('.hider').click(function(){
         var comment = $(this).parent().parent();
         if (comment.hasClass("mini")){
@@ -35,6 +40,19 @@ $(function(){
         } else {
             $(this).html("[ + ]");
             comment.addClass("mini");
+        }
+    });
+    // inlines pictures
+    $('.comment').each(function(){
+        var content = $(this).html();
+        var matches = content.match(/http.+\.(png|jpg|gif)/g);
+        if (matches) {
+            var unique = Array.from(new Set(matches));            
+            unique.forEach(function(element){
+                var newelem = "<img src=\"" + element + "\"/>";
+                content = content.replace(new RegExp(element, 'g'), newelem);
+            });
+            $(this).html(content);
         }
     });
 });
